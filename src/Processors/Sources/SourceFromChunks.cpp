8f5bebc5f6f2255be25ef5131d21a6ab2afb199c
Robert Schulze <robert@clickhouse.com>
2023-04-17 14:43:07 +0000

    (wip) QueryCache: Support totals and extremes
    Fixes: #48677
    
    Please don't review, still contains garbage

--- src/Processors/Sources/SourceFromChunks.cpp
+++ src/Processors/Sources/SourceFromChunks.cpp
@@ -1,27 +1,191 @@
 #include <Processors/Sources/SourceFromChunks.h>
 
+#include <QueryPipeline/StreamLocalLimits.h>
+
+
 namespace DB
 {
 
 SourceFromChunks::SourceFromChunks(Block header, Chunks chunks_)
-    : SourceFromChunks(header, std::make_shared<Chunks>(std::move(chunks_)), true)
-{}
+    : IProcessor({}, {header})
+    , output(*outputs.begin())
+    , chunks(std::make_shared<Chunks>(std::move(chunks_)))
+    , it(chunks->begin())
+    , move_from_chunks(true)
+{
+}
+
+SourceFromChunks::SourceFromChunks(Block header, Chunks chunks_, Chunks other, OtherTag tag)
+    : IProcessor({}, {header, header})
+    , output(*outputs.begin())
+    , output_totals(tag == OtherTag::Totals ? &*++outputs.begin() : nullptr)
+    , output_extremes(tag == OtherTag::Totals ? nullptr : &*++outputs.begin())
+    , chunks(std::make_shared<Chunks>(std::move(chunks_)))
+    , chunks_totals(tag == OtherTag::Totals ? std::make_shared<Chunks>(std::move(other)) : nullptr)
+    , chunks_extremes(tag == OtherTag::Totals ? nullptr : std::make_shared<Chunks>(std::move(other)))
+    , it(chunks->begin())
+    , move_from_chunks(true)
+{
+}
+
+SourceFromChunks::SourceFromChunks(Block header, Chunks chunks_, Chunks totals_, Chunks extremes_)
+    : IProcessor({}, {header, header, header})
+    , output(*outputs.begin())
+    , output_totals(&*++outputs.begin())
+    , output_extremes(&*++++outputs.begin())
+    , chunks(std::make_shared<Chunks>(std::move(chunks_)))
+    , chunks_totals(std::make_shared<Chunks>(std::move(totals_)))
+    , chunks_extremes(std::make_shared<Chunks>(std::move(extremes_)))
+    , it(chunks->begin())
+    , move_from_chunks(true)
+{
+}
 
 SourceFromChunks::SourceFromChunks(Block header, std::shared_ptr<Chunks> chunks_)
-    : SourceFromChunks(header, chunks_, false)
-{}
-
-SourceFromChunks::SourceFromChunks(Block header, std::shared_ptr<Chunks> chunks_, bool move_from_chunks_)
-    : ISource(std::move(header))
+    : IProcessor({}, {header})
+    , output(*outputs.begin())
     , chunks(chunks_)
     , it(chunks->begin())
-    , move_from_chunks(move_from_chunks_)
+    , move_from_chunks(false)
 {
+    chassert(chunks);
+}
+
+SourceFromChunks::SourceFromChunks(Block header, std::shared_ptr<Chunks> chunks_, std::shared_ptr<Chunks> other_, OtherTag tag)
+    : IProcessor({}, {header, header})
+    , output(*outputs.begin())
+    , output_totals(tag == OtherTag::Totals ? &*++outputs.begin() : nullptr)
+    , output_extremes(tag == OtherTag::Totals ? nullptr : &*++outputs.begin())
+    , chunks(chunks_)
+    , chunks_totals(tag == OtherTag::Totals ? other_ : nullptr)
+    , chunks_extremes(tag == OtherTag::Totals ? nullptr : other_)
+    , it(chunks->begin())
+    , move_from_chunks(false)
+{
+    chassert(chunks);
+    if (tag == OtherTag::Totals)
+        chassert(chunks_totals);
+    else
+        chassert(chunks_extremes);
+}
+
+SourceFromChunks::SourceFromChunks(Block header, std::shared_ptr<Chunks> chunks_, std::shared_ptr<Chunks> totals_, std::shared_ptr<Chunks> extremes_)
+    : IProcessor({}, {header, header, header})
+    , output(*outputs.begin())
+    , output_totals(&*++outputs.begin())
+    , output_extremes(&*++++outputs.begin())
+    , chunks(chunks_)
+    , chunks_totals(totals_)
+    , chunks_extremes(extremes_)
+    , it(chunks->begin())
+    , move_from_chunks(false)
+{
+    chassert(chunks);
+    chassert(chunks_totals);
+    chassert(chunks_extremes);
 }
 
 String SourceFromChunks::getName() const
 {
     return "SourceFromChunks";
+}
+
+SourceFromChunks::Status SourceFromChunks::prepare()
+{
+    // TODO
+    if (output_totals)
+        output_totals->finish();
+    if (output_extremes)
+        output_extremes->finish();
+
+    if (finished)
+    {
+        output.finish();
+        return Status::Finished;
+    }
+
+    /// Check can output.
+    if (output.isFinished())
+        return Status::Finished;
+
+    if (!output.canPush())
+        return Status::PortFull;
+
+    if (!has_input)
+        return Status::Ready;
+
+    output.pushData(std::move(current_chunk));
+    has_input = false;
+
+    if (isCancelled())
+    {
+        output.finish();
+        return Status::Finished;
+    }
+
+    if (got_exception)
+    {
+        finished = true;
+        output.finish();
+        return Status::Finished;
+    }
+
+    /// Now, we pushed to output, and it must be full.
+    return Status::PortFull;
+}
+
+void SourceFromChunks::work()
+{
+    try
+    {
+        read_progress_was_set = false;
+
+        if (auto chunk = generate())
+        {
+            current_chunk.chunk = std::move(chunk);
+            if (current_chunk.chunk)
+            {
+                has_input = true;
+                if (!read_progress_was_set)
+                    progress(current_chunk.chunk.getNumRows(), current_chunk.chunk.bytes());
+            }
+        }
+        else
+            finished = true;
+
+        if (isCancelled())
+            finished = true;
+    }
+    catch (...)
+    {
+        finished = true;
+        got_exception = true;
+        throw;
+    }
+}
+
+void SourceFromChunks::setStorageLimits(const std::shared_ptr<const StorageLimitsList> & /*storage_limits_*/)
+{
+    /// Should we bother?
+}
+
+void SourceFromChunks::progress(size_t read_rows, size_t read_bytes)
+{
+    read_progress_was_set = true;
+    read_progress.read_rows += read_rows;
+    read_progress.read_bytes += read_bytes;
+}
+
+std::optional<SourceFromChunks::ReadProgress> SourceFromChunks::getReadProgress()
+{
+    if (finished && read_progress.read_bytes == 0 && read_progress.total_rows_approx == 0)
+        return {};
+
+    ReadProgressCounters res_progress;
+    std::swap(read_progress, res_progress);
+
+    static StorageLimitsList empty_limits;
+    return ReadProgress{res_progress, empty_limits};
 }
 
 Chunk SourceFromChunks::generate()
--- src/Processors/Sources/SourceFromChunks.h
+++ src/Processors/Sources/SourceFromChunks.h
@@ -1,30 +1,58 @@
 #pragma once
 
-#include <Processors/Chunk.h>
-#include <Processors/ISource.h>
+#include <Processors/IProcessor.h>
 
 
 namespace DB
 {
 
-class SourceFromChunks : public ISource
+/// SourceFromSingleChunk' big brother wich supports multiple chunks and totals/extremes.
+class SourceFromChunks : public IProcessor
 {
 public:
+    /// Disambiguates the meaning of the "other" parameter in the ctor
+    enum class OtherTag
+    {
+        Totals,
+        Extremes
+    };
+
     SourceFromChunks(Block header, Chunks chunks_);
+    SourceFromChunks(Block header, Chunks chunks_, Chunks other, OtherTag tag);
+    SourceFromChunks(Block header, Chunks chunks_, Chunks totals_, Chunks extremes_);
+
     SourceFromChunks(Block header, std::shared_ptr<Chunks> chunks_);
+    SourceFromChunks(Block header, std::shared_ptr<Chunks> chunks_, std::shared_ptr<Chunks> other_, OtherTag tag);
+    SourceFromChunks(Block header, std::shared_ptr<Chunks> chunks_, std::shared_ptr<Chunks> totals_, std::shared_ptr<Chunks> extremes_);
+
+    ~SourceFromChunks() override = default;
 
     String getName() const override;
 
-protected:
-    Chunk generate() override;
+    Status prepare() override;
+    void work() override;
+
+    void setStorageLimits(const std::shared_ptr<const StorageLimitsList> & storage_limits_) override;
+    std::optional<ReadProgress> getReadProgress() final;
+    void progress(size_t read_rows, size_t read_bytes);
 
 private:
-    SourceFromChunks(Block header, std::shared_ptr<Chunks> chunks_, bool move_from_chunks_);
+    Chunk generate();
 
+    ReadProgressCounters read_progress;
+    bool read_progress_was_set = false;
+    OutputPort & output;
+    OutputPort * output_totals = nullptr;
+    OutputPort * output_extremes = nullptr;
+    bool has_input = false;
+    bool finished = false;
+    bool got_exception = false;
+    Port::Data current_chunk;
     const std::shared_ptr<Chunks> chunks;
-    Chunks::iterator it;
-    /// Optimization: if the chunks are exclusively owned by SourceFromChunks, then generate() can move from them
-    const bool move_from_chunks;
+    const std::shared_ptr<Chunks> chunks_totals;
+    const std::shared_ptr<Chunks> chunks_extremes;
+    Chunks::iterator it; // TODO multiple iterators
+    const bool move_from_chunks; /// Optimization: if the chunks are exclusively owned by SourceFromChunks, then generate() can move from them
 };
 
 }
